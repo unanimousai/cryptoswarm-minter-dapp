b3d4a3c87d00c4acd3cf7154b2ce6a6bac771c36
gregg-willcox <gregg@unanimousai.com>
2022-10-10 13:11:01 -0700

    add goerli functionality

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,13 +1,13 @@
 const contractAddress = "0x863EC143009C3e676EeA991Ab7B093806EA0EAD9"; // Replace with your own contract address
 const chain = 'goerli'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CryptoSwarm AI NFT Project";
+const welcome_h1 = "[TEST] Welcome to the CryptoSwarm AI NFT Project";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'CryptoSwarm AI is a collective Super Intelligence for predicting crypto assets using hundreds of human forecasters and the power of Swarm AI. For more information, visit our <a href="https://www.cryptoswarm.ai">website</a>.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h1_presale_coming_soon = "[TEST] NFT Drop Coming Soon!!";
+const h1_presale_mint = "[TEST] Pre-Sale Minting Open!!";
+const h1_public_mint = "[TEST] Public Minting Open!!";
+const h2_presale_coming_soon = "[TEST] Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
