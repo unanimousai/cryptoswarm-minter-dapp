9c8d0ee416674b93b5eb4fd8ed5c77676f07a032
gregg-willcox <gregg@unanimousai.com>
2022-10-14 11:20:09 -0700

    update twitter space link

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,13 +3,13 @@ const chain = 'ethereum'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CryptoSwarm AI NFT Project";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'CryptoSwarm AI is a collective Super Intelligence for predicting crypto assets using hundreds of human forecasters and the power of Swarm AI. For more information, visit our <a href="https://www.cryptoswarm.ai">website</a>. For help minting, see our <a href="https://www.cryptoswarm.ai/help">help page</a> or talk to us on our <a href="https://discord.com/invite/js4g9Xqa3K">discord</a>.<br><br>🎉 Join our <a href="https://twitter.com/i/spaces/1lPKqBQyggAGb">live Twitter space</a> for the mint party 🎉';
+const welcome_p = 'CryptoSwarm AI is a collective Super Intelligence for predicting crypto assets using hundreds of human forecasters and the power of Swarm AI. For more information, visit our <a href="https://www.cryptoswarm.ai">website</a>. For help minting, see our <a href="https://www.cryptoswarm.ai/help">help page</a> or talk to us on our <a href="https://discord.com/invite/js4g9Xqa3K">discord</a>.<br><br>🎉 Join our <a href="https://twitter.com/i/spaces/1nAKErXRRbAGL">live Twitter space</a> for the mint party 🎉';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon = 'We are working hard to launch the NFT Collection. Join our live Twitter space for the mint party 🎉: https://twitter.com/i/spaces/1nAKErXRRbAGL ';
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
